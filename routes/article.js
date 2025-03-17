64f16c9ed800f5cab2d8e9f5a6b3551b55bc6616
Aidan Benbow <aidanbenbow@gmail.com>
2025-03-17 10:23:25 +0200

    2nd

--- routes/article.js
+++ routes/article.js
@@ -1,8 +1,30 @@
 import express from 'express';
 const router = express.Router();
 
+import { createCheeseArticle, getAllCheeseArticles } from '../config/dynamoDB.js  ';
+
 router.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('articlesadd');
 });
+
+router.get('/showall', async (req, res) => {
+    const articles = await getAllCheeseArticles();
+console.log(articles);
+    res.render('showall', { articles: articles });
+});
+
+router.post('/submit', async (req, res) => {
+    const article = {
+        title: req.body.title,
+        createdAt: req.body.createdAt,
+        topic: req.body.topic,
+        description: req.body.description,
+        article: req.body.article
+    };
+  await createCheeseArticle(article);
+    res.redirect('/articles/showall');
+} );
+
+
 
 export default router;
